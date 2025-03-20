814774ddabdc07b2ef4bac1a6f5ef020a7c9c2a6
Peanuttttto <164983830+Peanuttttto@users.noreply.github.com>
2025-03-20 12:04:20 +0700

    Add files via upload

---
+++ 505d339cee193dc9c74a6c787dd4a2db.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.14 
+MySQL - 5.5.5-10.4.28-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `department` (
+	`dept_id` double ,
+	`dept_name` varchar (90)
+); 
+insert into `department` (`dept_id`, `dept_name`) values('001','IT');
+insert into `department` (`dept_id`, `dept_name`) values('002','HR');
+insert into `department` (`dept_id`, `dept_name`) values('003','ACC');
